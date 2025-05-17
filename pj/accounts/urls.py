7b31c976b7681768f44e2522567aaf5b5104076e
celsovssilva <celsoviniciussouzasilva@gmail.com>
2025-05-17 16:05:14 -0300

    tela de cadastrar usuario totalmente funcional

--- pj/accounts/urls.py
+++ pj/accounts/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from .  import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('hellen.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+   path('register/',views.registro.as_view(), name='registro' )
 ]
 
 
